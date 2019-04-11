37ef43d1987ced6ba039320a0acbfc9507e2fca2
programacion <diegoeauyonh@gmail.com>
2019-04-11 11:29:59 -0600

    WIP in form.

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -6,13 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario.component.scss']
 })
 export class FormularioComponent implements OnInit {
-  titulo = "Agregar un registro";
 
-  constructor() { 
-    
+  constructor() {
   }
 
   ngOnInit() {
   }
-
 }
