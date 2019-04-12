f5db8e28ae94c40dad42e355812b7cc32a4d0a7a
Donovan <donaleg12@gmail.com>
2019-04-11 19:10:13 -0600

    WIP

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Data } from '@angular/router';
 
 @Component({
   selector: 'app-formulario',
@@ -6,7 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario.component.scss']
 })
 export class FormularioComponent implements OnInit {
-
+  public nombre: string;
+  public segundoNombre: string;
+  public apellido: string;
+  public segundoApellido: string;
+  public apellidoCasada: string;
+  public fechaNacimiento: string;
+  public religión: string;
+  public email: string;
+  public genero: string;
+  public departamento: string;
+  public municipio: string;
+  public zona: string;
+  public colonia: string;
+  public avenida: string;
+  public calle: string;
+  public sector: string;
+  public numeroCasa: string;
+  public celular: number;
+  public domicilio: number;
+  public otro: number;
+  
   constructor() {
   }
 
