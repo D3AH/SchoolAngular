efbe2d24cbae8c07bd9eb444cecd6e564dab9d94
lewandowski7852 <diegoeauyonh@gmail.com>
2019-04-22 09:52:02 -0600

    Update model.

--- src/models/person.js
+++ src/models/person.js
@@ -1,28 +1,85 @@
-'use strict'
+const mongoose = require('mongoose');
 
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var PersonSchema = Schema({
-    firstName: String,
-    secondName: String,
-    firstSurname: String,
-    lastSurname: String,
-    birthdate: String,
-    religion: String,
-    email: String,
-    gender: String,
-    department: String,
-    municipality: String,
-    zone: String,
-    neighborhood: String,
-    avenue: String,
-    street: String,
-    area: String,
-    houseNumber: String,
-    mobileNumber: Number,
-    housePhone: Number,
-    another: Number
+const PersonSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: [true, 'The field firstName is required.']
+    },
+    secondName: {
+        type: String,
+        required: [true, 'The field secondName is required.']
+    },
+    firstSurname: {
+        type: String,
+        required: [true, 'The field firstSurname is required.']
+    },
+    lastSurname: {
+        type: String,
+        required: [true, 'The field lastSurname is required.']
+    },
+    birthdate: {
+        type: String,
+        required: [true, 'The field birthdate is required.']
+    },
+    religion: {
+        type: String,
+        required: [true, 'The field religion is required.']
+    },
+    email: {
+        type: String,
+        required: [true, 'The field email is required.']
+    },
+    gender: {
+        type: String,
+        required: [true, 'The field gender is required.']
+    },
+    department: {
+        type: String,
+        required: [true, 'The field department is required.']
+    },
+    municipality: {
+        type: String,
+        required: [true, 'The field municipality is required.']
+    },
+    zone: {
+        type: String,
+        required: [true, 'The field zone is required.']
+    },
+    neighborhood: {
+        type: String,
+        required: [true, 'The field neighborhood is required.']
+    },
+    avenue: {
+        type: String,
+        required: [true, 'The field avenue is required.']
+    },
+    street: {
+        type: String,
+        required: [true, 'The field street is required.']
+    },
+    area: {
+        type: String,
+        required: [true, 'The field area is required.']
+    },
+    houseNumber: {
+        type: String,
+        required: [true, 'The field houseNumber is required.']
+    },
+    mobileNumber: {
+        type: Number,
+        required: [true, 'The field mobileNumber is required.']
+    },
+    housePhone: {
+        type: Number,
+        required: [true, 'The field housePhone is required.']
+    },
+    another: {
+        type: Number,
+        required: [true, 'The field another is required.']
+    }
+}, {
+    // Localización: es, Intensidad de coincidencia
+    collation: { locale: 'es', strength: 3 }
 });
 
 module.exports = mongoose.model('Person', PersonSchema);
