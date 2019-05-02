8b7c1874d8d036116ed88fcbe09f8b1841d9c546
lewandowski7852 <diegoeauyonh@gmail.com>
2019-05-02 07:38:26 -0600

    Fixing navside.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +10,14 @@ export class AppComponent {
   events: string[] = [];
   opened: boolean;
 
+  constructor(private router: Router) {}
+
   toggle() {
     this.opened = !this.opened;
     console.log(this.opened);
-  }  
+  }
+
+  routeTo(route) {
+    this.router.navigate(['/agregar']);
+  }
 }
