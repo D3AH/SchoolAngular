eade073fd9caabbd831bb61f352f8c8f6744456e
lewandowski7852 <diegoeauyonh@gmail.com>
2019-05-09 11:59:22 -0600

    WIP

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -5,13 +5,15 @@ import { IndexComponent } from '../components/index/index.component';
 import { ListarPersonaComponent } from '../components/persona/listar-persona/listar-persona.component';
 import { AgregarTelefonoComponent } from '../components/persona/agregar-telefono/agregar-telefono.component';
 import { AgregarFamiliaComponent } from '../components/familia/agregar-familia/agregar-familia.component';
+import { ListarFamiliaComponent } from '../components/familia/listar-familia/listar-familia.component';
 
 const routes: Routes = [
   { path: '', component: IndexComponent },
   { path: 'persona/agregar', component: AgregarPersonaComponent },
   { path: 'persona/listar', component: ListarPersonaComponent },
   { path: 'persona/agregarTelefono', component: AgregarTelefonoComponent },
-  { path: 'familia/agregar', component: AgregarFamiliaComponent }
+  { path: 'familia/agregar', component: AgregarFamiliaComponent },
+  { path: 'familia/listar', component: ListarFamiliaComponent }
 ];
 
 @NgModule({
